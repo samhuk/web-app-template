9a9415700ac7f0e250453c165c5223826e785eb2
Sam Hughes <samhughes1701@hotmail.co.uk>
2022-01-02 01:05:17 +0000

    improve gzipper

--- buildScripts/gzipper.go
+++ buildScripts/gzipper.go
@@ -5,18 +5,24 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"os"
 )
 
 func main() {
 	// Gzip the large files
-	compressFile("./build/client/out.css")
-	compressFile("./build/client/out.js")
+	paths := [2]string{"./build/client/out.css", "./build/client/out.js"}
+	for i := 0; i < len(paths); i++ {
+		err := compressFile(paths[i])
+		if err != nil {
+			fmt.Printf("Error occured compressing file: %v. %v\n", paths[i], err)
+		}
+	}
 }
 
-func compressFile(filePath string) {
+func compressFile(filePath string) error {
 	// Read the text of the file
-	fileText, _ := os.ReadFile(filePath)
+	fileText, err := os.ReadFile(filePath)
 	// Gzip the text
 	var buf bytes.Buffer
 	zw := gzip.NewWriter(&buf)
@@ -24,4 +30,5 @@ func compressFile(filePath string) {
 	zw.Close()
 	// Wrie the gzipped data to file, in same directory with '.gz' suffix
 	os.WriteFile(filePath+".gz", buf.Bytes(), 0666)
+	return err
 }
